a0a74abb3441573f474b4988a5f32a719308b0e4
7ssan91 <a7med_7ssan@hotmail.com>
2019-12-27 15:56:45 +0200

    filer doctors

--- src/app/shared/filter/filter.component.ts
+++ src/app/shared/filter/filter.component.ts
@@ -1,60 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { BaseService } from 'src/services/core/base.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { DoctorsService } from 'src/services/doctor/doctor.service';
-import { Doctor, Specialities, City, Area } from 'src/services/models/models';
-import { SpecialityService } from 'src/services/speciality/speciality.service';
-import { HomeService } from 'src/services/home/home.service';
+import { Component, OnInit } from "@angular/core";
+import { BaseService } from "src/services/core/base.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { DoctorsService } from "src/services/doctor/doctor.service";
+import { Doctor, Specialities, City, Area } from "src/services/models/models";
+import { SpecialityService } from "src/services/speciality/speciality.service";
+import { HomeService } from "src/services/home/home.service";
 
 @Component({
-  selector: 'filter',
-  templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  selector: "filter",
+  templateUrl: "./filter.component.html",
+  styleUrls: ["./filter.component.scss"]
 })
 export class FilterComponent implements OnInit {
   cities: City[] = [];
   citiesTemp: City[] = [];
-  doctors:Doctor[]=[];
+  doctors: Doctor[] = [];
   selectedArea: Area = new Area();
   selectedCity: City = new City();
-  selectedSpe:Specialities=new Specialities();
-  specialities:Specialities[]=[]
-  searchKey='';
-  depth:number =1;
+  specialities: Specialities[] = [];
+  selectedspecialty: Specialities =new Specialities();
+
+  searchKey = "";
   area: Area = new Area();
   specialitiesTemp?: Specialities[] = [];
-  constructor(public base: BaseService, private home:HomeService ,private sp:SpecialityService, private route: Router, http: HttpClient,private doc:DoctorsService, private acroute:ActivatedRoute) { 
-  }
+  constructor(
+    public base: BaseService,
+    private home: HomeService,
+    private sp: SpecialityService,
+    private route: Router,
+    http: HttpClient,
+    private doc: DoctorsService,
+    private acroute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.sp.getSpeciality().subscribe((data)=>{
-      this.specialities=data;
+    this.sp.getSpeciality().subscribe(data => {
+      this.specialities = data;
     });
-    this.doc.getDoctors().subscribe((data)=>{
-      this.doctors=data;
+    this.doc.getDoctors().subscribe(data => {
+      this.doctors = data;
     });
   }
- getCity(){
-  this.home.getAvilableCities().subscribe((data) => { this.cities = data; Object.assign(this.citiesTemp, data);
+  getCity() {
+    this.home.getAvilableCities().subscribe(data => {
+      this.cities = data;
+      Object.assign(this.citiesTemp, data);
+    });
+  }
+  isSlectedArea(Id) {
+    return this.base.area.getValue().Id == Id;
+  }
+  selectCity(cit: City) {
+    this.selectedCity = cit;
+  }
+  selectArea(ar: Area) {
+    this.selectedArea = ar;
+    this.sp.getSpecialitiesForArea(ar.Id).subscribe((data: Specialities[]) => {
+      this.specialities = data;
+    });
+  }
+ dox:Doctor[]=[];
+ areaId=-1;
+  search(){
+    // this.selectArea(ar)
+    // this.areaId = this.base.area.getValue().Id;
+    console.log( this.selectedArea.Id);
+    this.selectedArea.Id;
 
-  })
- };
- isSlectedArea(Id) {
-  return this.base.area.getValue().Id == Id;
-}
-
-
-
-selectCity(cit: City) {
-  this.selectedCity = cit;
-  this.depth = 2;
-};
-selectArea(area:any) {
-  console.log(area)
-  this.sp.getSpecialitiesForArea(this.area).subscribe((data: Specialities[]) => {
-    this.specialities = data;
-    this.specialitiesTemp = data;
-    console.log(data)
-  });}
+    this.doc.getDoctors(this.searchKey,this.selectedspecialty.Id,1,this.selectedArea.Id).subscribe((searchDoc)=>{
+      this.dox=searchDoc;
+      console.log(this.dox)
+    })
+  }
 }
